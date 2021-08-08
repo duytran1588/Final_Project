f41f0b2c24cef6075d84ae4dcec29b189226a7fc
DuyTran <duy88tran@gmail.com>
2021-08-08 18:37:35 +0700

    update_search

--- cinema/src/pages/home/home.jsx
+++ cinema/src/pages/home/home.jsx
@@ -6,15 +6,15 @@ import News from "./news/news";
 import MobileApp from "./mobile-app/mobile-app";
 import IconMoving from "../../components/icon_moving/icon_to_top";
 import SelectMovie from "./select-movie/select_Movie";
-import SelectMovieName from "./select-movie-name/select_Movie_Name";
+
 
 function Home() {
   return (
     <div>
       <IconMoving />
       <Carousel />
-      {/* <SelectMovie /> */}
-      <SelectMovieName/>
+      <SelectMovie />
+      {/* <SelectMovieName/> */}
       <ComingMovie />
       <CinemaInfo />
       <News />
